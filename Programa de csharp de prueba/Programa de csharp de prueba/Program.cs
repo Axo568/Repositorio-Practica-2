0328db8ba493580179880971b6ca41e8acb5de59
Axo568 <adanca02@ucm.es>
2026-02-09 11:43:33 +0100

    Añadi el resto de archivos iniciales

---
+++ Programa de csharp de prueba/Programa de csharp de prueba/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programa_de_csharp_de_prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
